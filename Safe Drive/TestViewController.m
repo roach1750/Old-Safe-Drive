490a4e2245f8faeb20a39e641e73efaaeb9ca700
Andrew Roach <roach1750@gmail.com>
2016-04-28 16:24:44 -0400

    added BACtrack

--- Safe Drive/TestViewController.m
+++ Safe Drive/TestViewController.m
@@ -7,18 +7,30 @@
 //
 
 #import "TestViewController.h"
+#import "BACtrack.h"
 
-@interface TestViewController ()
+@interface TestViewController () <BacTrackAPIDelegate>
+@property (strong, nonatomic) BacTrackAPI *mbacTrack;
+@property (weak, nonatomic) IBOutlet UIButton *connectButton;
 
 @end
 
 @implementation TestViewController
 
 - (void)viewDidLoad {
-    
+    self.mbacTrack = [[BacTrackAPI alloc] initWithDelegate:self AndAPIKey:@"0a32f2ab7bf144e4905e723347989b"];
     [super viewDidLoad];
 }
 
+-(void)BacTrackAPIKeyDeclined:(NSString *)errorMessage	{
+    NSLog(@"API KEY Doesn't work: %@",errorMessage);
+}
 
+- (IBAction)connectButtonPressed:(UIButton *)sender {
+    [self.mbacTrack connectToNearestBreathalyzer];
+}
 
+-(void)BacTrackConnected{
+    NSLog(@"connected");
+}
 @end
